8240771448ab2b5641ccb0354528fbfd42d5e08f
Gabriel Abreu <gabriel.abreu9913@gmail.com>
2024-05-27 19:42:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+/* WELCOME ----------------------------------------------------------------------------------------------------------------------- */
+var typed = new Typed(".text", {
+	strings: [
+		"Backend Developer",
+		"Software Optimizer",
+		"API Developers",
+		"System Analyst",
+	],
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true,
+});
+/* ABOUT ----------------------------------------------------------------------------------------------------------------------- */
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+var sidemenu = document.getElementById("sidemenu");
+
+function opentab(tabname) {
+	for (tablink of tablinks) {
+		tablink.classList.remove("active-link");
+	}
+	for (tabcontent of tabcontents) {
+		tabcontent.classList.remove("active-tab");
+	}
+	event.currentTarget.classList.add("active-link");
+	document.getElementById(tabname).classList.add("active-tab");
+}
+
+function openmenu() {
+	sidemenu.style.right = "0";
+}
+function closemenu() {
+	sidemenu.style.right = "-200px";
+}
+/* SERVICES ----------------------------------------------------------------------------------------------------------------------- */
